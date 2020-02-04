101956f93fc68513e75797e2f562ae396e9f507f
christian.sauer <christian.sauer@codecentric.de>
2020-02-04 21:29:28 +0100

    docs; custom resolver for logfiles, regression in partials

---
+++ NactuatorSample/Nactuator/Logfile/ILogFileLocationResolver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NetCoreAdmin.Logfile
+{
+    public interface ILogFileLocationResolver
+    {
+        /// <summary>
+        /// Implement this to resolve the file path of the log File at runtime. This can also return null, but that disables all log viewing functionality.
+        /// This function is called everytime a log file is requested
+        /// When both this and a LogFilePath are configured, this Resolver takes precedence.
+        /// </summary>
+        /// <returns></returns>
+        public string ResolveLogFileLocation();
+    }
+}
---
+++ NactuatorSample/NactuatorSample/SerilogLogResolver.cs
@@ -0,0 +1,26 @@
+﻿using NetCoreAdmin.Logfile;
+using System.IO;
+using System.Linq;
+
+namespace NetCoreAdminSample
+{
+    public class SerilogLogResolver : ILogFileLocationResolver
+    {
+        public string ResolveLogFileLocation()
+        {
+            var logFolder = Directory.GetCurrentDirectory() + "/log";
+
+            var dInfo = new DirectoryInfo(logFolder);
+            var files = dInfo.GetFiles();
+
+            var sorted = files.OrderByDescending(x => x.LastWriteTimeUtc);
+            var newest = sorted.FirstOrDefault();
+            if (newest != null)
+            {
+                return newest.FullName;
+            }
+
+            return null!;
+        }
+    }
+}
