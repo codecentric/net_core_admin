e4430c4dd88786c6ede2250f2f6d5a3e58818b01
christian.sauer <christian.sauer@codecentric.de>
2020-02-11 12:03:58 +0100

    adding thread infos

--- NactuatorSample/Nactuator/Metrics/BasicMetricsProvider.cs
+++ NactuatorSample/Nactuator/Metrics/BasicMetricsProvider.cs
@@ -13,17 +13,101 @@ namespace NetCoreAdmin.Metrics
         private readonly ISystemStatisticsProvider systemStatisticsProvider;
         private readonly Dictionary<string, Func<MetricsData>> resolvers;
 
-        public BasicMetricsProvider(ISimpleEventListener eventListener, ISystemStatisticsProvider systemStatisticsProvider )
+        public BasicMetricsProvider(ISimpleEventListener eventListener, ISystemStatisticsProvider systemStatisticsProvider)
         {
             // list of providers especially for SBA who expects this names
             resolvers = new Dictionary<string, Func<MetricsData>>() {
                 {"process.uptime", GetProcessUptime },
                 {"process.cpu.usage", GetProcessCPUUsage },
-                {"system.cpu.usage", GetSystemCPUUsage }
+                {"system.cpu.usage", GetSystemCPUUsage },
+                {"system.cpu.count", GetCPUCount },
+                {"jvm.threads.live", GetLiveThreads },
+                {"jvm.threads.daemon", GetDaemonThreads },
+                {"jvm.threads.peak", GetPeakThreads }
             };
 
             this.eventListener = eventListener ?? throw new ArgumentNullException(nameof(eventListener));
             this.systemStatisticsProvider = systemStatisticsProvider;
+        }
+
+        private int peakThreads = 0;
+
+        private MetricsData GetPeakThreads()
+        {
+            var threadCount = Process.GetCurrentProcess().Threads.Count;
+
+            if (threadCount > peakThreads)
+            {
+                peakThreads = threadCount;
+            }
+
+            var result = new List<Measurement>()
+            {
+                new Measurement
+                {
+                    Statistic = "VALUE", Value = peakThreads
+                }
+            };
+
+            return new MetricsData()
+            {
+                Name = "jvm.threads.peak",
+                BaseUnit = "threads",
+                Description = "The peak live thread count since the .Net Core runtime started or peak was reset",
+                Measurements = result
+            };
+        }
+
+        private MetricsData GetDaemonThreads()
+        {
+
+            var result = new List<Measurement>()
+            {
+                new Measurement
+                {
+                    Statistic = "VALUE", Value = 0
+                }
+            };
+
+            return new MetricsData()
+            {
+                Name = "jvm.threads.daemon",
+                BaseUnit = "threads",
+                Description = "The current number of live daemon threads - always 0 since .Net does not have the concept",
+                Measurements = result
+            };
+        }
+
+        private MetricsData GetLiveThreads()
+        {
+
+            var threads = Process.GetCurrentProcess().Threads;
+            var result = new List<Measurement>()
+            {
+                new Measurement
+                {
+                    Statistic = "VALUE", Value =threads.Count
+                }
+            };
+
+            return new MetricsData()
+            {
+                Name = "jvm.threads.live",
+                BaseUnit = "threads",
+                Description = "The current number of live threads. This are system threads, not managed threads",
+                Measurements = result
+            };
+        }
+
+        private MetricsData GetCPUCount()
+        {
+            return new MetricsData()
+            {
+                Name = "system.cpu.count",
+                BaseUnit = null!,
+                Description = "The number of processors available to the .Net Core Runtime",
+                Measurements = new List<Measurement>() { new Measurement { Statistic = "VALUE", Value = System.Environment.ProcessorCount } }
+            };
         }
 
         private MetricsData GetSystemCPUUsage()
@@ -50,7 +134,7 @@ namespace NetCoreAdmin.Metrics
                 value = 0.0001;
             }
 
-            var fallBack = new List<Measurement>() { new Measurement() { Statistic = "VALUE", Value = value } }; 
+            var fallBack = new List<Measurement>() { new Measurement() { Statistic = "VALUE", Value = value } };
 
             return new MetricsData()
             {
@@ -63,14 +147,16 @@ namespace NetCoreAdmin.Metrics
 
         public async Task<MetricsData> GetMetricByNameAsync(string name)
         {
-            if (resolvers.TryGetValue(name, out var resultFunc)){
+            if (resolvers.TryGetValue(name, out var resultFunc))
+            {
                 return resultFunc();
             }
 
-            if (eventListener.Metrics.TryGetValue(name, out var result)){
+            if (eventListener.Metrics.TryGetValue(name, out var result))
+            {
                 return result;
             }
-            
+
             throw new KeyNotFoundException(name);
         }
 
@@ -90,7 +176,7 @@ namespace NetCoreAdmin.Metrics
                 Name = "process.uptime",
                 BaseUnit = "seconds",
                 Description = "The uptime of the process",
-                Measurements = new List<Measurement>() { new Measurement() { Statistic = "VALUE", Value = uptime.TotalSeconds} }
+                Measurements = new List<Measurement>() { new Measurement() { Statistic = "VALUE", Value = uptime.TotalSeconds } }
             };
         }
     }
